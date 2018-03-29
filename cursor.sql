c21e0b224ef33610d9a50089c9eb4de6171d725c
Matiasruizr <matiasruizr@hotmail.es>
2018-03-28 22:32:30 -0300

    Método for para utilizar cursores

--- cursor.sql
+++ cursor.sql
@@ -31,7 +31,46 @@ BEGIN
     DBMS_OUTPUT.PUT_LINE(emp_dep10v2.last_name);
     fetch c1 into emp_dep10v2;-- Colocamos el cursor en un valor mas abajo (Casi como un ++)
   end loop;
-  
-  close cl; -- Debemos cerrarlo para poder ejecutarlo nuevamente
+  close c1; -- Debemos cerrarlo para poder ejecutarlo nuevamente
+END;
+/
+
+
+--codigo 2
+DECLARE
+  emp_dep10v2 DEPARTAMENTO_10V2%rowtype; --adopta los valores de departamento_10
+  cursor c1 is  select first_name, last_name  --Cursor es un arreglo que trae los resultados de la consulta fila por fila
+                from employees
+               where Department_id = 10;
+BEGIN
+  open c1; --Con esto abrimos el cursor antes de un loop (Es como inicializar), recien a este momento ejecuta el select (definido arriba)
+   loop --Loop repite una accion indefinidamente, hasta que un exit lo indique
+   fetch c1 into emp_dep10v2; --fetch , coloca en la siguiente fila de resultados (si es por primera vez, inicializa 1)
+    exit when c1%notfound; --Salir cuando no encuentre resultados
+    insert into DEPARTAMENTO_10V2 values emp_dep10v2;
+    DBMS_OUTPUT.PUT('Nombre: '); --El put deja mensajes en cola, cuando ingresamos el putline lo imprimimos
+    DBMS_OUTPUT.PUT(emp_dep10v2.first_name);
+    DBMS_OUTPUT.PUT(' ');
+    DBMS_OUTPUT.PUT_LINE(emp_dep10v2.last_name);
+  end loop;
+  close c1; -- Debemos cerrarlo para poder ejecutarlo nuevamente
+END;
+/
+
+--cursor 3, por medio de for
+-- El for crea la variable fila, la cual es un rowtype automatica, esta existira solo cuando recorre el for
+-- por cada fila hace un fetch, abre y cierra el cursor de manera automatica
+DECLARE
+  cursor c1 is  select first_name, last_name  --Cursor es un arreglo que trae los resultados de la consulta fila por fila
+                from employees
+                where Department_id = 10;
+BEGIN
+   FOR FILA IN c1 LOOP -- CLONA LOS RESULTADOS EN FILA, Y RECORRELOS, ABRE Y CIERA CURSOR DE MANERA AUTOMATICA
+    insert into DEPARTAMENTO_10V2 values FILA;
+    DBMS_OUTPUT.PUT('Nombre: '); --El put deja mensajes en cola, cuando ingresamos el putline lo imprimimos
+    DBMS_OUTPUT.PUT(FILA.first_name);
+    DBMS_OUTPUT.PUT(' ');
+    DBMS_OUTPUT.PUT_LINE(FILA.last_name);
+   END LOOP;
 END;
 /
